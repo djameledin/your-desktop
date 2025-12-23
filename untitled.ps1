dcc8f0b9272dc01273ec011fd9f8be85439e23f9
DjamelEddine <eddine29@icloud.com>
2025-12-23 11:55:15 +0100

    Update untitled.ps1

--- untitled.ps1
+++ untitled.ps1
@@ -109,7 +109,7 @@ Clean-Desktop
 
 Download-Wallpaper -URL $wallURL -Path $wallPath
 
-reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+New-Item 'HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32' -Force | Out-Null; Set-ItemProperty $_.PSPath '(Default)' ''
 
 Stop-Process -Name explorer -Force -ErrorAction SilentlyContinue
 Start-Process explorer.exe
