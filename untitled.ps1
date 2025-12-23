d0e6884a3f7bb78cf5c213b72634aa1cd3c1f935
DjamelEddine <eddine29@icloud.com>
2025-12-23 11:35:19 +0100

    Update untitled.ps1

--- untitled.ps1
+++ untitled.ps1
@@ -109,6 +109,8 @@ Clean-Desktop
 
 Download-Wallpaper -URL $wallURL -Path $wallPath
 
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
 Stop-Process -Name explorer -Force -ErrorAction SilentlyContinue
 Start-Process explorer.exe
 
